90183f4f67ac0b41d61b1462bb946c1216105709
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-12-05 13:32:39 -0300

    Adiciona atividade SequenciaCentena

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _numeroNatural = default;
+            string _cadeiaNumeros = default;
+
+            Console.WriteLine("------ Sequência Centena ------\n");
+
+            while(_numeroNatural != 100)
+            {
+                _numeroNatural ++;
+                _cadeiaNumeros += $" {_numeroNatural}";
+            }
+
+            Console.WriteLine("{0}",_cadeiaNumeros);
+        }
+    }
+}
